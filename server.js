c53daac3a87e4d42a3eefef6f398f32944e52c87
Beata Karolczak <karolczak.beata@gmail.com>
2018-03-30 01:40:46 +0200

    Add mlab database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://beata:qweqwe@ds227939.mlab.com:27939/database-1', {
     useMongoClient: true
 });
 
