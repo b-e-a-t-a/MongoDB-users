bede900a415feb3e66cf90ce28cfebe414244d21
Beata Karolczak <karolczak.beata@gmail.com>
2018-03-29 16:13:57 +0200

    Add new users

--- server.js
+++ server.js
@@ -3,37 +3,85 @@ const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/nodeappdatabase', {
-	useMongoClient: true
+    useMongoClient: true
 });
 
 //new user Schema
 const userSchema = new Schema({
-	name: String,
-	username: { type: String, required: true, unique: true},
-	password: { type: String, required: true},
-	admin: Boolean,
-	created_at: Date,
-	updated_at: Date
+    name: String,
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    admin: Boolean,
+    created_at: Date,
+    updated_at: Date
 });
-
-const User = mongoose.model('User', userSchema);
 
 //mongoose schema method
 userSchema.methods.manify = function(next) {
-	this.name = this.name + '-boy';
+    this.name = this.name + '-boy';
 
-	return next(null, this.name);
+    return next(null, this.name);
 };
 
 //pre-save method
 userSchema.pre('save', function(next) {
-	const currentDate = new Date();
+    const currentDate = new Date();
 
-	this.updated_at = currentDate;
+    this.updated_at = currentDate;
 
-	if (!this.created_at) {
-		this.created_at = currentDate;
-	}
-	
-	next();
+    if (!this.created_at)
+        this.created_at = currentDate;
+
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+const kenny = new User({
+    name: 'Kenny',
+    username: 'Kenny_the_boy',
+    password: 'password'
+});
+
+kenny.manify(function(err, name) {
+    if (err) throw err;
+    console.log('Twoje nowe imię to: ' + name);
+});
+
+kenny.save(function(err) {
+    if (err) throw err;
+    console.log('Uzytkownik ' + kenny.name +' zapisany pomyslnie');
+});
+
+
+const benny = new User({
+	name: 'Benny',
+	username: 'Benny_the_boy',
+	password: 'password'
+});
+
+benny.manify(function(err, name) {
+	if (err) throw err;
+	console.log('Twoje nowe imię to: ' + name);
+});
+
+benny.save(function(err) {
+	if (err) throw err;
+	console.log('Uzytkownik ' + benny.name + ' zapisany pomyslnie');
+});
+
+const mark = new User ({
+	name: 'Mark',
+	username: 'Mark_the_boy',
+	password: 'password'
+});
+
+mark.manify(function(err, name) {
+	if (err) throw err;
+	console.log('Twoje nowe imię to: ' + name);
+});
+
+mark.save(function(err) {
+	if (err) throw (err);
+	console.log('Uzytkownik ' + mark.name + ' zapisany pomyslnie');
 });
